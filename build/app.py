da6df152bca67482aadf52cbbe6c897a54d22c2f
TroyNelson11 <trnelson@redhat.com>
2022-06-16 20:08:31 +0000

    Initial streamlit test

---
+++ build/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import subprocess
+
+def ttsoutput(textinput):
+    t= textinput
+    #subprocess.run(['tts', "--text", "\"" + t + "\"", '--model_name',"tts_models/en/ljspeech/glow-tts", '--vocoder_name', "vocoder_models/universal/libri-tts/fullband-melgan"])
+    output = open('text-to-speech/tts_output.wav', 'rb')
+    st.text(t)
+    st.audio(output)
+    return
+
+ttsoutput(st.text_input('Say Something'))
