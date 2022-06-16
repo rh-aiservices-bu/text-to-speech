164ba08b3c21afa459206a533649867315fa0e59
TroyNelson11 <trnelson@redhat.com>
2022-06-16 20:29:50 +0000

    added tts command to streamlit app

--- build/app.py
+++ build/app.py
@@ -3,7 +3,7 @@ import subprocess
 
 def ttsoutput(textinput):
     t= textinput
-    #subprocess.run(['tts', "--text", "\"" + t + "\"", '--model_name',"tts_models/en/ljspeech/glow-tts", '--vocoder_name', "vocoder_models/universal/libri-tts/fullband-melgan"])
+    subprocess.run(['tts', "--text", "\"" + t + "\"", '--model_name',"tts_models/en/ljspeech/glow-tts", '--vocoder_name', "vocoder_models/universal/libri-tts/fullband-melgan"])
     output = open('text-to-speech/tts_output.wav', 'rb')
     st.text(t)
     st.audio(output)
